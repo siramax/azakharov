84d6c666ed0813f65db8eef670899f44c54ce322
aazaharov81 <aazaharov81@16087486-b754-11de-b129-73e2b2a9d7e9>
2010-05-16 19:17:21 +0000

    Added sudoku solver

---
+++ scripts/sudoku/sudoku.en.js
@@ -0,0 +1,25 @@
+/**
+ * Codepage dependent resources
+ */
+m = {
+    buttons: {
+        solve:  { label: 'Solve', note: "The decision may take some time (from several seconds to 5 minutes on slower machines)", },
+        step:   { label: 'Step' },
+        help:   { label: 'What is this?' },
+        stats:  { label: 'Stats' },
+        clear:  { label: 'Clear' },
+        diag:   { label: 'Consider the diagonal' },
+        import: { label: 'Import' },
+        export: { label: 'Export' }
+    },
+    inputNumber: "Input the number for cell ",
+    inputEN: "Input the number for this cell ", //!< For Empty Number
+    errors: {
+        empty:                                          "Empty!",
+        linesNotEqual:      function( y, w ) { return   "Row number not equal " + w + "!" ; },
+        columnsNotEqual:    function( y, w ) { return   "Columns number at row "+( y+1 )+" not equal " + w + "!"; },
+        notFound:                                       "Cannot find target field",
+        noField:                                        "There is no field"
+    }
+}
+    
